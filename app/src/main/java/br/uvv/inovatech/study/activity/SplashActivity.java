fb5aa32b8bac752c022acd6332e27031282868e3
kelvinxs <kelvinxisto2006@gmail.com>
2017-04-12 20:03:58 -0300

    First commit

---
+++ app/src/main/java/br/uvv/inovatech/study/activity/SplashActivity.java
@@ -0,0 +1,19 @@
+package br.uvv.inovatech.study.activity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import br.uvv.inovatech.study.R;
+
+public class SplashActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+
+    public void onEnterAppButtonClicked(View view){
+
+    }
+}
