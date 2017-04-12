77297ddda3f7a2f18bf06f2e60103c458b53be54
kelvinxs <kelvinxisto2006@gmail.com>
2017-04-12 20:13:32 -0300

    First commit

---
+++ app/src/main/java/br/uvv/inovatech/study/object/GrupoEstudo.java
@@ -0,0 +1,13 @@
+package br.uvv.inovatech.study.object;
+
+import java.util.Date;
+import java.util.List;
+
+public class GrupoEstudo {
+    public Long id;
+    public String nome;
+    public List<Usuario> membros;
+    public Date quando;
+    public String local;
+    public String descricao;
+}
---
+++ app/src/main/java/br/uvv/inovatech/study/object/Usuario.java
@@ -0,0 +1,9 @@
+package br.uvv.inovatech.study.object;
+
+public class Usuario {
+    public Long id;
+    public String token;
+    public String nome;
+    public String matricula;
+    public String senha;
+}
